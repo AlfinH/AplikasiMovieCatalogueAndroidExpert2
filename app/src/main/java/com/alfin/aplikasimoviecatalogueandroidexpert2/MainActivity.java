0f8fe451feb9600df6410fa11a26f003168e73fb
AlfinH <47842410+AlfinH@users.noreply.github.com>
2019-12-08 21:41:09 +0700

    Setting Menu (Ganti Bahasa)
    English dan Indonesia

--- app/src/main/java/com/alfin/aplikasimoviecatalogueandroidexpert2/MainActivity.java
+++ app/src/main/java/com/alfin/aplikasimoviecatalogueandroidexpert2/MainActivity.java
@@ -1,9 +1,15 @@
 package com.alfin.aplikasimoviecatalogueandroidexpert2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -21,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
 
         getSupportActionBar().setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_change_settings){
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
